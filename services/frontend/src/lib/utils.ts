c0007d003102778dc3229623ad006cbe9bc99991
blan22 <jsp@marpple.com>
2024-11-05 19:03:10 +0900

    chore: update named export

--- services/frontend/src/lib/utils.ts
+++ services/frontend/src/lib/utils.ts
@@ -1,5 +1,3 @@
 import { html } from 'rune-ts';
 
-const klasses = <T extends any>(...args: T[]) => html`${args.filter(Boolean).join(' ')}`;
-
-export { klasses };
+export const klasses = <T extends any>(...args: T[]) => html`${args.filter(Boolean).join(' ')}`;
