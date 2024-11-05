96c1a83b6ebb578d01ce93554dafcc94d3cf72ec
blan22 <jsp@marpple.com>
2024-11-05 16:15:56 +0900

    feat: impl header, layout unit component

---
+++ services/frontend/src/components/unit/layout/index.ts
@@ -0,0 +1,32 @@
+import { html, View, type Html } from 'rune-ts';
+import klass from './layout.module.scss';
+
+interface LayoutData {
+  content?: View | Html;
+}
+
+interface LayoutOptions {
+  header?: View | Html;
+  footer?: View | Html;
+}
+
+class Layout extends View<LayoutData> {
+  constructor(
+    data: LayoutData,
+    public options: LayoutOptions = {},
+  ) {
+    super({ ...data });
+  }
+
+  override template() {
+    return html`
+      <div class="${klass.layout}">
+        ${this.options.header ? html`<header>${this.options.header}</header>` : ''}
+        <main>${this.data.content}</main>
+        ${this.options.footer ? html`<footer>${this.options.footer}</footer>` : ''}
+      </div>
+    `;
+  }
+}
+
+export { Layout };
