9cc665aea3c17e5de44829c650c10c8340cc659b
blan22 <jsp@marpple.com>
2024-11-06 17:59:46 +0900

    feat: add ChangeImageUploaderFileEvent on Image Uploader component

--- services/frontend/src/components/cell/uploader/image.ts
+++ services/frontend/src/components/cell/uploader/image.ts
@@ -1,12 +1,24 @@
-import { html, View } from 'rune-ts';
+import { html, on, View, CustomEventWithDetail } from 'rune-ts';
+import { pipe, reduce, take, toArray } from '@fxts/core';
 import klass from './image.module.scss';
-import { Button } from '../../atom';
+import { Button, Typography } from '../../atom';
+
+export class ChangeImageUploaderFileEvent extends CustomEventWithDetail<File | null> {}
 
 class ImageUploader extends View<{}> {
+  private _thumbnail: File | null = null;
+
   override template() {
     return html`
       <div class="${klass.uploader}">
-        <p>image.png</p>
+        <div class="${klass.name}">
+          ${this._thumbnail
+            ? html`${new Typography({ text: this._thumbnail.name })}`
+            : html`${new Typography(
+                { text: '썸네일을 첨부해주세요.' },
+                { as: 'span', color: 'GRAY_50', weight: 'MEDIUM', size: 'SIZE_16' },
+              )}`}
+        </div>
         <div>
           <label for="thumbnail">
             <span>썸네일 첨부</span>
@@ -17,6 +29,33 @@ class ImageUploader extends View<{}> {
         </div>
       </div>
     `;
+  }
+
+  @on('change')
+  onChange(e) {
+    if (e.target.id === 'thumbnail') {
+      this._thumbnail = pipe(
+        toArray(e.target.files),
+        take(1),
+        reduce((value: File) => value),
+      );
+
+      this.dispatchEvent(ChangeImageUploaderFileEvent, { detail: this._thumbnail, bubbles: true });
+      this.redraw();
+    }
+  }
+
+  @on('click')
+  _reset(e) {
+    if (!this._thumbnail) return;
+    const button = this.subView(Button);
+
+    if (e.target === button?.element()) {
+      this._thumbnail = null;
+
+      this.dispatchEvent(ChangeImageUploaderFileEvent, { detail: this._thumbnail, bubbles: true });
+      this.redraw();
+    }
   }
 }
 
